30173888d25d234eb45e4272bdfada8e93c53cf6
Paola Bashige <paolabashige@gmail.com>
2022-08-10 19:25:53 +0300

    initial commit

---
+++ src/config/config.ts
@@ -0,0 +1,23 @@
+export const config = {
+    "dev": {
+      "username": "myudards",
+      "password": "myudards",
+      "database": "myudards",
+      "host": "myudards.cwp5abk1g4nr.us-east-1.rds.amazonaws.com",
+      "dialect": "postgres",
+      "aws_region": "us-east-1",
+      "aws_profile": "default",
+      "aws_media_bucket": "mydacitybucket"
+    },
+    "jwt": {
+      "secret": " "
+    },
+    "prod": {
+      "username": "myudards",
+      "password": "myudards",
+      "database": "myudards",
+      "host": "myudards.cwp5abk1g4nr.us-east-1.rds.amazonaws.com",
+      "dialect": "postgres"
+    }
+  }
+  
